4c42537a9ea680efcd1e10a28b23e5034b468d36
Micah J. Martin <mjm5097@rit.edu>
2018-04-26 03:54:22 -0400

    Cleaned up sample file

--- examples/detfile.py
+++ examples/detfile.py
@@ -4,10 +4,10 @@ detcord : Action execution on hosts
 Micah Martin - knif3
 '''
 
-from detcord.commands import action, run, local, put, get, display, script
+from detcord.commands import action, local, put, get, display, script
 
 
-env = {}
+env = {}  # pylint: disable=invalid-name
 env['pass'] = 'root'
 env['user'] = 'toor'
 env['hosts'] = ['192.168.177.161']
@@ -19,19 +19,22 @@ def test():
 
     Showcases the commands that can be run
     '''
-    # You can pass a script to be piped into another program
-    ret = script("bash", "echo this is valid\nThis is an error")
-
+    # You can run simple commands
+    ret = script("echo welcome to detcord")
+    # You can pass a script to be piped into the process
+    ret = script("bash", "echo this is valid\nThis is an error\n")
+    # You can run a command as root
+    ret = script("whoami", sudo=True)
     # Display can handle the direct output of a command
     display(ret)
-    # You can call simple actions on the remote host
-    # You can pull out the direct stdout and stderr
+    # Run commands locally
     ret = local("whoami")
     # Display can handle stdout and stderr
     display(ret)
     # Put and push files to/from the server
     put("../README.md", "/tmp/README")
     get("/tmp/README", "test.swp")
+
 
 def support_action():
     '''
