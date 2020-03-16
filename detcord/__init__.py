c7bf9c54de446378cb3a9e244c41774869e79cd2
micahjmartin <mjm5097@rit.edu>
2020-03-15 23:43:48 -0400

    Fixed potential error in display

--- detcord/__init__.py
+++ detcord/__init__.py
@@ -26,9 +26,9 @@ def display(obj, **kwargs):
     Pretty print the output of an action
     """
     host = obj.get("host", "")
-    for line in obj["stdout"].strip().split("\n"):
+    for line in obj.get("stdout", "").strip().split("\n"):
         if line:
             print("[{}] [+]:".format(host), line, **kwargs)
-    for line in obj["stderr"].strip().split("\n"):
+    for line in obj.get("stderr", "").strip().split("\n"):
         if line:
             print("[{}] [-]:".format(host), line, **kwargs)
